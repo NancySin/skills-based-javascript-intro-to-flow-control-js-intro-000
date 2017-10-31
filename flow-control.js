a74ae280cbe543171c9e3919d0ecf67d9f4b4edb
nancy sin <nsin@sbcglobal.net>
2017-10-31 23:50:30 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,22 @@
-function basicTeenager(age) {
 
-}
+function basicTeenager(age) {
+  if (age >=13 && <=19) {
+    return "You are a teenager!";
+   }
+ }
 
 function teenager(age) {
 
 }
 
 function ageChecker(age) {
-
+  if (age >=13 && <=19) {
+    return "You are a teenager!";
+  } else if (age <= 12) {
+    return "You are a kid";
+  } else {
+    return "You are a grownup";
+  }
 }
 
 function ternaryTeenager(age) {
